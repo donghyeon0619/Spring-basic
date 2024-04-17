5e74bc08323715e63518a7e9ef3b0235e09f2557
DongHyeon Yoon <128972134+donghyeon0619@users.noreply.github.com>
2024-04-17 11:31:52 +0900

    [feat] ObjectProvider, Provider 코드 구현

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -2,11 +2,14 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+import org.springframework.test.context.event.annotation.AfterTestClass;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -33,22 +36,35 @@ public class SingletonWithPrototypeTest {
         assertThat(clientBean1.logic()).isEqualTo(1);
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
-        assertThat(clientBean2.logic()).isEqualTo(2);
+        assertThat(clientBean2.logic()).isEqualTo(1);
 
     }
+
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;  //생성 시점에 주입됨
+
+        /*
+        @Autowired
+        private ObjectProvider<PrototypeBean> objectProvider;  //생성 시점에 주입됨
+        */
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> objectProvider;     // 스프링부트3 부터 javax.inject가 아닌 jakarta.inject를 사용
 
         public int logic(){
+            /*
+            getObject()에 매개변수에는 아무것도 안들어가도됨 왜냐하면 처음 ObjectProvider를 생성할 때
+            그 클래스에 대한 빈을 만들어주는 것으로 설정하고 만들었으므로
+            자바에서는 for문에 사용하는 라벨 느낌인듯 es a:for
+            */
+
+//          PrototypeBean prototypeBean = objectProvider.getObject(); ObjectProvider를 사용할 때 사용
+            PrototypeBean prototypeBean = objectProvider.get();
+
             prototypeBean.addCount();
-//            int count = prototypeBean.getCount();     둘이 합치는 단축키 ctrl + alt + n
-//            return count;
+
+//          int count = prototypeBean.getCount();     둘이 합치는 단축키 ctrl + alt + n
+//          return count;
             return prototypeBean.getCount();
         }
     }
